8ae85fe91cd91424eb3dd89c03af43f5b64dd651
Marek Novotný <marek.novotny@h2o.ai>
2020-02-10 21:33:03 +0100

    Fix scala style

--- core/src/main/scala/org/apache/spark/h2o/backends/external/RestCommunication.scala
+++ core/src/main/scala/org/apache/spark/h2o/backends/external/RestCommunication.scala
@@ -188,7 +188,7 @@ trait RestCommunication extends Logging {
     endpoint.resolve(suffixWithDelimiter).toURL
   }
 
-  private def setHeaders(connection: HttpURLConnection, conf: H2OConf, requestType: String,  params: Map[String, Any]): Unit = {
+  private def setHeaders(connection: HttpURLConnection, conf: H2OConf, requestType: String, params: Map[String, Any]): Unit = {
     getCredentials(conf).foreach(connection.setRequestProperty("Authorization", _))
 
     if (params.nonEmpty && (requestType == "POST" || requestType == "PUT")) {
@@ -237,7 +237,7 @@ trait RestCommunication extends Logging {
     }
   }
 
-  private def throwRestApiNotReachableException(url: URL,  e: Exception) = {
+  private def throwRestApiNotReachableException(url: URL, e: Exception) = {
     throw new RestApiNotReachableException(
       s"""External H2O node ${urlToString(url)} is not reachable.
          |Please verify that you are passing ip and port of existing cluster node and the cluster
